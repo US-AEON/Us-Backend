37ecbb176bcc8c77239ce526e9f95f1b832fcab3
myunsyeya <myunsyeya@ajou.ac.kr>
2025-06-28 02:12:32 +0900

    feat: Add conversation-flow api.

--- src/speech/speech.module.ts
+++ src/speech/speech.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { SpeechController } from './speech.controller';
-import { SpeechService } from './speech.service';
 import { SpeechToTextService } from './services/speech-to-text.service';
 import { TextToSpeechService } from './services/text-to-speech.service';
+import { ConversationService } from './services/conversation.service';
+import { GeminiModule } from '../integrations/gemini/gemini.module';
 
 @Module({
+  imports: [GeminiModule],
   controllers: [SpeechController],
-  providers: [SpeechService, SpeechToTextService, TextToSpeechService],
-  exports: [SpeechService],
+  providers: [SpeechToTextService, TextToSpeechService, ConversationService],
+  exports: [ConversationService],
 })
 export class SpeechModule {}
