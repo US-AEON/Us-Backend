159ac110554095298e0498969c922e048918de17
LeeWxx <dnckd0903@naver.com>
2025-06-27 21:10:33 +0900

    feat: Add Gemini translation service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FirebaseModule } from './firebase/firebase.module';
+import { GeminiModule } from './integrations/gemini/gemini.module';
 
 @Module({
-  imports: [FirebaseModule],
+  imports: [FirebaseModule, GeminiModule],
   controllers: [AppController],
   providers: [AppService],
 })
